320fca196619c06e7fbf1febfaec933b70fb9e43
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-09-13 20:28:09 +0900

    112: feat: develop getVisited graphQl apis

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -80,6 +80,16 @@ export class PropertyResolver {
 	}
 
 	@UseGuards(AuthGuard)
+	@Query((returns) => Properties)
+	public async getVisited(
+		@Args('input') input: OrdinaryInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query:, getVisited');
+		return await this.propertyService.getVisited(memberId, input);
+	}
+
+	@UseGuards(AuthGuard)
 	@Mutation(() => Property)
 	public async likeTargetProperty(
 		@Args('propertyId') input: string,
